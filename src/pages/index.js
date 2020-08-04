e1cf3b69a99a9e91fa01467e60cad862f7d9cd67
mikewiner <winer.mike@gmail.com>
2020-08-03 21:19:52 -0400

    h1 change

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <div className='flex items-center tc vh-100 pa0 '>
       <div className='w-70 center v-mid ml3 mr3 br3 white' >
-        <h1 className="text-shadow">The cleanest water bottle ever invented.</h1>
+        <h1 className="text-shadow">The smart travel bottle.</h1>
         <p className="text-shadow">Sign up now for pre-launch updates and early bird pricing.</p>
         <p className="text-shadow">Click the link below for more info.</p>  
         <Link className="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-dark-blue" to="/signup">Click here</Link>
